639c5655727df91b1ef3869cdace3b8313f99f53
Ihor Rud <ihor.rud@onix-systems.com>
2025-03-07 13:07:27 +0200

    fix: surround paginate() with try-catch and throw ModelNotFoundException to prevent 500 server error

--- src/Model/Builder.php
+++ src/Model/Builder.php
@@ -9,13 +9,16 @@ declare(strict_types=1);
 
 namespace OnixSystemsPHP\HyperfCore\Model;
 
+use Exception;
 use Hyperf\Database\Model\Builder as BaseBuilder;
 use Hyperf\Database\Model\Collection;
 use Hyperf\Database\Model\Model;
+use Hyperf\Database\Model\ModelNotFoundException;
 use OnixSystemsPHP\HyperfCore\DTO\Common\PaginationRequestDTO;
 use OnixSystemsPHP\HyperfCore\DTO\Common\PaginationResultDTO;
 use OnixSystemsPHP\HyperfCore\Model\Filter\AbstractFilter;
 use OnixSystemsPHP\HyperfCore\Repository\HasRepository;
+use function Hyperf\Translation\__;
 
 /**
  * @method Builder filter(AbstractFilter $filters)
@@ -24,15 +27,12 @@ class Builder extends BaseBuilder
 {
     use HasRepository;
 
-    private const ORDER_ASC = 'asc';
-
-    private const ORDER_DESC = 'desc';
-
     public function __call($method, $parameters): mixed
     {
         if (isset($this->repository) && method_exists($this->repository, $scope = 'scope' . ucfirst($method))) {
             return $this->callScope([$this->repository, $scope], $parameters);
         }
+
         return parent::__call($method, $parameters);
     }
 
@@ -58,62 +58,69 @@ class Builder extends BaseBuilder
         if ($result instanceof Model || is_null($result)) {
             return $result;
         }
+
         return null;
     }
 
     public function get($columns = ['*']): array|Collection
     {
         $this->guardList();
+
         return parent::get($columns);
     }
 
     public function chunk($count, callable $callback)
     {
         $this->guardList();
+
         return parent::chunk($count, $callback);
     }
 
     public function chunkById($count, callable $callback, $column = null, $alias = null)
     {
         $this->guardList();
+
         return parent::chunkById($count, $callback, $column, $alias);
     }
 
     public function each(callable $callback, $count = 1000)
     {
         $this->guardList();
+
         return parent::each($callback, $count);
     }
 
     public function paginateDTO(PaginationRequestDTO $params): PaginationResultDTO
     {
         $this->guardList();
-        $page = $params?->page ?? 1;
+        $page = $params->page ?? 1;
         $per_page = $params?->per_page ?? 20;
-        if (is_array($params?->order)) {
+        $by = 'ASC';
+        if (is_array($params->order)) {
             foreach ($params->order as $item) {
                 if (str_starts_with($item, '-')) {
-                    $by = self::ORDER_DESC;
-                    $field = substr($item, 1);
+                    $by = 'DESC';
+                    $column = substr($item, 1);
                 } else {
-                    $by = self::ORDER_ASC;
-                    $field = $item;
+                    $column = $item;
                 }
-                $this->orderBy($field, $by);
+                $this->orderByRaw("$column $by NULLS LAST");
             }
         }
-        $paginated = $this->paginate(
-            perPage: $per_page,
-            page: $page,
-        );
-        $total = $paginated->total();
-        return PaginationResultDTO::make([
-            'list' => $paginated->items(),
-            'total' => $total,
-            'page' => $page,
-            'per_page' => $per_page,
-            'total_pages' => $per_page != 0 ? ceil($total / $per_page) : 0,
-        ]);
+        try {
+            $paginated = $this->paginate($per_page, page: $page);
+            $total = $paginated->total();
+
+            return PaginationResultDTO::make([
+                'list' => $paginated->items(),
+                'total' => $total,
+                'page' => $page,
+                'per_page' => $per_page,
+                'total_pages' => $per_page != 0 ? ceil($total / $per_page) : 0,
+            ]);
+        } catch (Exception $e) {
+            throw new ModelNotFoundException(__('exceptions.model.pagination.failed'));
+        }
     }
 
     protected function guardList(): void
